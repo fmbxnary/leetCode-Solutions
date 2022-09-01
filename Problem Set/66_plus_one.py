1fd7e022a8175e7c2fbeafa945abd10443ff37fa
Fatih Mehmet Baydar <baydarmf@gmail.com>
2022-09-01 19:49:53 +0300

    Update 66_plus_one.py

--- Problem Set/66_plus_one.py
+++ Problem Set/66_plus_one.py
@@ -1,3 +1,5 @@
-digits = [1, 2, 3]
-a = ''.join([str(n) for n in digits])
-print(a)
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = ''.join([str(n) for n in digits])
+        number = list(str(int(number) + 1))
+        return number
